a246bea33dd2b34019fadda4c34dcd7f8c1358cf
Tru00 <ttzimina0@gmail.com>
2024-01-06 21:11:57 -0800

    added facade changes

--- generic/helpers/account.py
+++ generic/helpers/account.py
@@ -1,8 +1,12 @@
 from apis.dm_api_account.models.registration_model import RegistrationModel
+try:
+    from services.dm_api_account import Facade
+except ImportError:
+    ...
 
 
 class Account:
-    def __init__(self, facade):
+    def __init__(self, facade: Facade):
         self.facade = facade
 
     def set_headers(self, headers):
